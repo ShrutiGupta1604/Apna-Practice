00992956c967f0845f2c5e61e9d8787c95b7957a
ShrutiGupta <ShrutiGupta1604@gmail.com>
2024-07-31 19:06:17 +0530

    DOM event Listener

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
-// practice Question 
-// let h1 = document.createElement("h1");
-// h1.innerHTML= "<u>DOM preactice </u>" ;
-// document.querySelector("body").append(h1);
-// h1.classList.add("red");
+// ----practice Question--------
+
+let h1 = document.createElement("h1");
+h1.innerHTML= "<u>DOM preactice </u>" ;
+document.querySelector("body").append(h1);
+h1.classList.add("red");
+
 //-------------------------------------------------------
+
+//--Generate Random color--
+
+ 
