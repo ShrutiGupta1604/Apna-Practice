d2c8fd060c20314fcbfa5d7a2c3579f9e1955dd3
ShrutiGupta <ShrutiGupta1604@gmail.com>
2024-07-31 18:47:44 +0530

    DOM event

---
+++ app.js
@@ -0,0 +1,6 @@
+// practice Question 
+// let h1 = document.createElement("h1");
+// h1.innerHTML= "<u>DOM preactice </u>" ;
+// document.querySelector("body").append(h1);
+// h1.classList.add("red");
+//-------------------------------------------------------
