2af7a2e85f382cdf9a5042e1999bdde9b5cdb3ae
ShrutiGupta <ShrutiGupta1604@gmail.com>
2024-08-03 20:27:19 +0530

    Visualizing the call stack

--- app.js
+++ app.js
@@ -1,12 +1,39 @@
 // ----practice Question--------
 
-let h1 = document.createElement("h1");
-h1.innerHTML= "<u>DOM preactice </u>" ;
-document.querySelector("body").append(h1);
-h1.classList.add("red");
+// let h1 = document.createElement("h1");
+// h1.innerHTML= "<u>DOM preactice </u>" ;
+// document.querySelector("body").append(h1);
+// h1.classList.add("red");
 
 //-------------------------------------------------------
 
-//--Generate Random color--
+//---- *Visualizing the call stack *----------
 
- 
+// function one() {
+//     return 1;
+// }
+
+// function two(){
+//     return one() + one();
+// }
+// function three(){
+//     let ans = two() + one();
+//     console.log(ans) 
+// }
+// three(); 
+
+//--------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
